9aa1e32fb8f8bf7a859cc54587f2e73c4d63cb6e
88sahil <sahilmaliya59@gmail.com>
2024-06-12 09:00:03 +0530

    access

--- server/index.js
+++ server/index.js
@@ -40,6 +40,12 @@ App.use(express.json())
 App.use(cookieparser())
 
 App.use(cors())
+App.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 App.use(ratelimit({
     window:60*1000*60,
     limit:100
